168080a5654154edec85fd8b2bc29ed26b6bf4da
Edevaldo Junior <ed.tadsifpr@gmail.com>
2022-03-04 16:42:55 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/edevaldojr/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/edevaldojr/cursomc/repositories/ProdutoRepository.java
@@ -1,13 +1,11 @@
 package com.edevaldojr.cursomc.repositories;
 
-import com.edevaldojr.cursomc.domain.Categoria;
+import com.edevaldojr.cursomc.domain.Produto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoriaRepository  extends JpaRepository<Categoria, Integer> {
-
-
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
     
 }
