1bd86d19ff18c91dd6a036728ea746c7c486ff3d
Edevaldo Junior <ed.tadsifpr@gmail.com>
2022-03-04 17:17:43 -0300

    Ajustes no endpoint /categorias/id

--- src/main/java/com/edevaldojr/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/edevaldojr/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,5 @@
 package com.edevaldojr.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import com.edevaldojr.cursomc.domain.Categoria;
 import com.edevaldojr.cursomc.services.CategoriaService;
@@ -21,11 +19,8 @@ public class CategoriaResource {
     private CategoriaService service;
 
     @RequestMapping(value="/{id}", method=RequestMethod.GET)
-    public ResponseEntity<?> find(@PathVariable Integer id) {
-       
-
+    public ResponseEntity<?> find(@PathVariable Integer id) {  
         Categoria obj = service.buscar(id);
-        
         return ResponseEntity.ok().body(obj);
     }
 }
